de50d1e79b62c03fcc24a274e1e78be18c0e61bb
SikhMeme <rajslikhari@gmail.com>
2021-12-01 23:19:24 -0600

    WEB

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const TwitchController = require('./controller/TwitchController.js')
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://queueup-front.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
